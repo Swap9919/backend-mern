5a80a10280e8872e0333e15d6b09690e7c1f60d2
Sai Swaroop <saiswaroop874@gmail.com>
2022-11-01 18:51:34 +0300

    splice and slice

--- day 2/index.js
+++ day 2/index.js
@@ -1,1 +1,41 @@
-console.log("Starting again...");
+// function constructor
+// function Person(name) {
+//   this.name = name;
+//   this.age = 21;
+// }
+// var object = new Person("Sudheer");
+// console.log(object); 
+
+// object litteral syntax
+// var object = {
+//       name: "Sudheer",
+//       age: 21
+// };
+// console.log(object);
+
+// array slice() method;  its returns the selected array elements as a new array object.
+// let arrayIntegers = [1, 2, 3, 4, 5];
+// let arrayIntegers1 = arrayIntegers.slice(0, 4); // returns [1,2]
+// let arrayIntegers2 = arrayIntegers.slice(2, 4); // returns [3]
+// let arrayIntegers3 = arrayIntegers.slice(0); //returns [5]
+// console.log(arrayIntegers1);
+// console.log(arrayIntegers2);
+// console.log(arrayIntegers3);
+
+// array splice() method; it return the deleted array and changes the original array
+// let arrayIntegersOriginal1 = [1, 2, 3, 4, 5];
+// let arrayIntegersOriginal2 = [1, 2, 3, 4, 5];
+// let arrayIntegersOriginal3 = [1, 2, 3, 4, 5];
+
+// let arrayIntegers1 = arrayIntegersOriginal1.splice(0, 2); 
+// returns [1, 2]; original array: [3, 4, 5]
+// let arrayIntegers2 = arrayIntegersOriginal2.splice(3); 
+// returns [4, 5]; original array: [1, 2, 3]
+// let arrayIntegers3 = arrayIntegersOriginal3.splice(3, 1, "a", "b", "c"); 
+//returns [4]; original array: [1, 2, 3, "a", "b", "c", 5]
+
+var of = ['of'];
+for(var of of of) {
+  console.log(of);
+}
+
